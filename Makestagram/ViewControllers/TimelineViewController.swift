f2d1e7e736d43c1912351ff0138ef442544d314c
Kazu Komoto <kazuhiro.komoto@students.makeschool.com>
2015-11-20 14:43:30 -0800

    Modified

--- Makestagram/ViewControllers/TimelineViewController.swift
+++ Makestagram/ViewControllers/TimelineViewController.swift
@@ -10,26 +10,34 @@ import UIKit
 
 class TimelineViewController: UIViewController {
 
+    var photoTakingHelper: PhotoTakingHelper?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.tabBarController?.delegate = self
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: Tab Bar Delegate
+
+extension TimelineViewController: UITabBarControllerDelegate {
+        
+    func tabBarController(tabBarController: UITabBarController, shouldSelectViewController viewController: UIViewController) -> Bool {
+        if (viewController is PhotoViewController) {
+            takePhoto()
+            return false
+        } else {
+            return true
+        }
+            
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func takePhoto() {
+     // instantiate photo taking class, provide callback for when photo is selected
+        let photoTakingHelper = PhotoTakingHelper(viewController: self.tabBarController!) { (image: UIImage?) in
+            print("received a callback")
+            // don't do anthing, yet...
+        }
     }
-    */
-
 }
